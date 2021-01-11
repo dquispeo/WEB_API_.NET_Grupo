95b2e060c354f74f59c2cdcf9b968f01389d1359
dquispeo <dquispe.oscco.97@gmail.com>
2021-01-11 17:38:44 -0500

    first commit

---
+++ Infraestructure/Migrations/202101050253580_v2.cs
@@ -0,0 +1,24 @@
+﻿namespace Infraestructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v2 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Students", "studentLastName");
+            DropColumn("dbo.Students", "studentCode");
+            DropColumn("dbo.Students", "startDate");
+            DropColumn("dbo.Students", "EnrollmentDate");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Students", "EnrollmentDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Students", "startDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Students", "studentCode", c => c.String(nullable: false));
+            AddColumn("dbo.Students", "studentLastName", c => c.String(nullable: false));
+        }
+    }
+}
---
+++ MVCAJAX/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿//using Domain;
+using MVCAJAX.Models;
+//using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAJAX.Controllers
+{
+    public class StudentController: Controller
+    {
+        Proxy.StudentProxy proxy = new Proxy.StudentProxy();
+
+        // GET: Student
+        public ActionResult IndexRazor()
+        {
+            var response = Task.Run(() => proxy.GetStudentAsync());
+            return View(response.Result.listado);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getStudent(string id)
+        {
+            var response = Task.Run(() => proxy.GetStudentAsync());
+            return Json(response.Result.listado, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult createStudent(StudentModel std)
+        {
+            //service.Insert(std);
+            var response = Task.Run(() => proxy.InsertAsync(std));
+            string message = response.Result.Mensaje;
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+
+        /*[HttpPost]
+        public JsonResult updateStudent(Student std, int id)
+        {
+            service.Update(std, id);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+
+        [HttpPost]
+        public JsonResult deleteStudent(int id)
+        {
+            service.Delete(id);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }*/
+    }
+}
