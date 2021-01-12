2e0256ddcad16eea3bc467f69a0e7617b22abe73
dquispeo <dquispe.oscco.97@gmail.com>
2021-01-11 19:10:54 -0500

    Four Coomit

---
+++ MVCAJAX/Models/FacturaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAJAX.Models
+{
+    public class FacturaModel
+    {
+        public int facturaID { get; set; }
+        public int numeroFactura { get; set; }
+        public DateTime fechaFactura { get; set; }
+        public string nombreVendedor { get; set; }
+        public string nombreCliente { get; set; }
+        public double subTotal { get; set; }
+        public double IGV { get; set; }
+        public double total { get; set; }
+        public bool estado { get; set; }
+    }
+}
---
+++ MVCAJAX/Models/ProductoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAJAX.Models
+{
+    public class ProductoModel
+    {
+        public int productoID { get; set; }
+        public string codigo { get; set; }
+        public double precio { get; set; }
+        public int stok { get; set; }
+        public bool enabled { get; set; }
+    }
+}
